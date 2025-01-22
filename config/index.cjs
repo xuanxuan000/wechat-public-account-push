b4a9609385c8509bd8ab669516e2f7a2182f230b
zhzhao15 <zhzhao15@iflytek.com>
2025-01-22 15:13:00 +0800

    test

--- config/index.cjs
+++ config/index.cjs
@@ -8,20 +8,19 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
+  APP_ID: 'wx01ca1abf943e7875',
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'f0e1320921fe72de1b4ac856fd7dee6c',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '安徽',
+  CITY: '合肥',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '凝凝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oh9VL7A1FX_Y9mlAmvusQo7o2ILg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +56,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oh9VL7A1FX_Y9mlAmvusQo7o2ILg',
     }
   ],
 
