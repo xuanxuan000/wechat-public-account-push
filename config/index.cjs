f84cefaa18db2da834362e551ebd9b7f6bd2ba58
zhzhao15 <zhzhao15@iflytek.com>
2025-01-22 15:23:32 +0800

    test

--- config/index.cjs
+++ config/index.cjs
@@ -50,7 +50,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'UkH8gOCf5b4aJhuo6yx_uiYWY-3-vjRa88fsBE-eWJA',
 
   CALLBACK_USERS: [
     {
