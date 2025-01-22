6a3191fa78619fdc2c58a407ed78243b27ab5a21
zhzhao15 <zhzhao15@iflytek.com>
2025-01-22 15:25:52 +0800

    test

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oh9VL7A1FX_Y9mlAmvusQo7o2ILg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'UkH8gOCf5b4aJhuo6yx_uiYWY-3-vjRa88fsBE-eWJA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
