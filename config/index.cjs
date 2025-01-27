3d7e1df3085c226b8905bf36b765f0516d4b2732
xuanxuan000 <46336868+xuanxuan000@users.noreply.github.com>
2025-01-27 23:45:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oh9VL7A1FX_Y9mlAmvusQo7o2ILg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'dtWtVzJ_t108dLkVCoGA_o4THMwuMEo8iJCx8kuS99M',
+      useTemplateId: 'VY35_ydKExePtVI0hF2wNnlYKJKgPpL92OyvemwtYuo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
